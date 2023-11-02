b28ebd3f52209340e6e16a6d377df36d541afa38
Tianqi Yang <tpy5064@psu.edu>
2023-11-02 13:29:32 -0400

    added system APIs

---
+++ src/main/java/edu/bu/met/cs665/system/CustomerData_HTTPS.java
@@ -0,0 +1,6 @@
+package edu.bu.met.cs665.system;
+
+public interface CustomerData_HTTPS {
+    String printCustomer(int customerId);
+    String getCustomer_HTTPS(int customerId);
+}
---
+++ src/main/java/edu/bu/met/cs665/system/CustomerData_USB.java
@@ -0,0 +1,6 @@
+package edu.bu.met.cs665.system;
+
+public interface CustomerData_USB {
+    String printCustomer(int customerId);
+    String getCustomer_USB(int customerId);
+}
